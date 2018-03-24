d1b278ff8242ec54f7cca0556b95781644faac79
Max Veytsman <mveytsman@github.com>
2018-03-24 18:09:11 -0400

    Move always

--- lib/sshnakes/player.ex
+++ lib/sshnakes/player.ex
@@ -6,7 +6,7 @@ defmodule SSHnakes.Player do
 
   use GenServer
 
-  defstruct [:game, :tty_pid]
+  defstruct [:game, :tty_pid, :direction]
 
   # API
   def start_link(args) do
@@ -22,22 +22,26 @@ defmodule SSHnakes.Player do
   def init(_args) do
     {:ok, tty_pid} = GenServer.start_link(TTY, self)
     game = Game.new
-
-    send(self, :print_position)
-    {:ok, %State{game: game, tty_pid: tty_pid}}
+    direction = Enum.random([:up, :right, :down, :left])
+    send(self, :tick)
+    {:ok, %State{game: game, tty_pid: tty_pid, direction: direction}}
   end
 
-  def handle_cast({:move, direction}, %State{game: game} = state) do
-    state = %{state | game: Game.move_player(game, direction)}
-    {:noreply, state}
+  def handle_cast({:move, direction}, state) do
+    case direction do
+    direction
+      when direction in [:up, :right, :down, :left]
+      -> {:noreply, %{state | direction: direction}}
+    _ ->   {:noreply, state}
+    end
   end
 
-  def handle_info(:print_position, state) do
+  def handle_info(:tick, state) do
     size = TTY.get_size(state.tty_pid)
     viewport = Game.get_viewport(state.game, size)
     IO.write Formatter.format_game(viewport)
-    Process.send_after(self, :print_position, 50)
-    {:noreply, state}
+    Process.send_after(self, :tick, 50)
+    {:noreply, %{state | game: Game.move_player(state.game, state.direction)}}
   end
 
 end
