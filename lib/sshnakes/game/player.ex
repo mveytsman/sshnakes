ae27f29130525c86e3678f9623ee2b677131388e
Max Veytsman <mveytsman@github.com>
2018-03-24 22:49:58 -0400

    Dead code

--- lib/sshnakes/game/player.ex
+++ lib/sshnakes/game/player.ex
@@ -1,11 +1,6 @@
 defmodule SSHnakes.Game.Player do
   require Logger
-  alias SSHnakes.TTY
-  alias SSHnakes.Formatter
-  alias SSHnakes.Game
   alias __MODULE__
-
-  use GenServer
 
   @directions [:up, :right, :down, :left]
 
@@ -39,31 +34,4 @@ defmodule SSHnakes.Game.Player do
   defp random_direction do
     Enum.random(@directions)
   end
-
-  # Implementation
-
-  def init(_args) do
-    {:ok, tty_pid} = GenServer.start_link(TTY, self)
-    player = %{SSHnakes.Game.spawn_player| tty_pid: tty_pid}
-    send(self, :tick)
-    {:ok, player}
-  end
-
-  def handle_cast({:move, direction}, state) do
-    case direction do
-    direction
-      when direction in [:up, :right, :down, :left]
-      -> {:noreply, %{state | direction: direction}}
-    _ -> {:noreply, state}
-    end
-  end
-
-  def handle_info(:tick, state) do
-    size = TTY.get_size(state.tty_pid)
-    viewport = Game.get_viewport(state.game, size)
-    IO.write Formatter.format_game(viewport)
-    Process.send_after(self, :tick, 50)
-    {:noreply, %{state | game: Game.move_player(state.game, state.direction)}}
-  end
-
 end
